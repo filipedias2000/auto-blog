642163e9f918deada928fae3859139b83cf3f3c0
Filipe Dias <filipe.dias@wtvision.com>
2025-12-05 20:37:33 +0000

    Remove typescript references from backend

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
